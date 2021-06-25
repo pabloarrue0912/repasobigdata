98ba3485c1c5f2946068a978012e417cd253d5b3
pabloarrue0912 <pabloarruepena@gmail.com>
2021-06-25 15:00:12 -0400

    Create tipodedatos.R
    repaso datos

---
+++ tipodedatos.R
@@ -0,0 +1,17 @@
+
+
+
+#gato es para comentarios
+#enteros, 0 1 2 3
+#doubles 0,11   2,3
+#boleanos true false t f
+#string y caracteres
+"hola mundo"
+
+
+#creamos la variable
+
+hola <- "pasto"
+hola
+hola <- "oveja"
+hola
